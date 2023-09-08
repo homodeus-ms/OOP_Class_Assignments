209b69edb5dd11f049d862bc85f9587573afd6f9
X1_LAPTOP\seekc <seekchange@daum.net>
2023-09-09 08:25:02 +0900

    과제1 3-3 추가 약간 수정

--- Assignment1/src/academy/pocu/comp2500/assignment1/Post.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/Post.java
@@ -1,7 +1,10 @@
 package academy.pocu.comp2500.assignment1;
 
 import java.time.OffsetDateTime;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Post {
     private String title;
