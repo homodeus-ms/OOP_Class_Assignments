8ffb0f9f02b92e66a9de507912f41893f578ded2
X1_LAPTOP\seekc <seekchange@daum.net>
2023-09-27 16:24:32 +0900

    Revert "Revert "과제2 스탬프 + 컬러""
    This reverts commit ff6e4da0cd241f93924326caf8425105047aa128.

--- Assignment2/src/academy/pocu/comp2500/assignment2/StampType.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/StampType.java
@@ -1,7 +1,35 @@
 package academy.pocu.comp2500.assignment2;
 
-public enum StampType {
-    RED_STAMP,
-    GREEN_STAMP,
-    BLUE_STAMP
+public class StampType {
+
+    private StampColor color;
+    private StampSize size;
+
+    public StampType(StampColor color, StampSize size) {
+        this.color = color;
+        this.size = size;
+    }
+
+    public StampColor getColor() {
+        return this.color;
+    }
+
+    /*public ColorType getColor() {
+        switch (this.color) {
+            case RED:
+                return ColorType.RED;
+            case GREEN:
+                return ColorType.GREEN;
+            case BLUE:
+                return ColorType.BLUE;
+            default:
+                assert (false);
+                return ColorType.CUSTOM;
+        }
+    }*/
+
+    public StampSize getSize() {
+        return size;
+    }
+
 }
