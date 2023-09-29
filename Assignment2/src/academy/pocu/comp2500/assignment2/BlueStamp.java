1b28403a47c3038913e4cab0093e30e3d162ff8b
X1_LAPTOP\seekc <seekchange@daum.net>
2023-09-29 15:11:47 +0900

    과제2 다시 Stamp의 자식클래스 생성

---
+++ Assignment2/src/academy/pocu/comp2500/assignment2/BlueStamp.java
@@ -0,0 +1,7 @@
+package academy.pocu.comp2500.assignment2;
+
+public class BlueStamp extends Stamp {
+    public BlueStamp(StampSize size, String text) {
+        super(size, RGB.BLUE, text);
+    }
+}
---
+++ Assignment2/src/academy/pocu/comp2500/assignment2/RedStamp.java
@@ -0,0 +1,7 @@
+package academy.pocu.comp2500.assignment2;
+
+public class RedStamp extends Stamp {
+    public RedStamp(StampSize size, String text) {
+        super(size, RGB.RED, text);
+    }
+}
