9a12b25278417a1df17b1131ce04f2cb73eff2e2
X1_LAPTOP\seekc <seekchange@daum.net>
2023-10-01 16:32:06 +0900

    과제2 밑의 것 매개변수 수정

--- Assignment2/src/academy/pocu/comp2500/assignment2/BusinessCard.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/BusinessCard.java
@@ -55,14 +55,14 @@ public class BusinessCard extends Product {
 
 
     public BusinessCard(BusinessCardType cardType, BusinessCardSides sides, BusinessCardColor color,
-                        PrintOrientation orientation, ArrayList<TextAperture> textApertures,
-                        ArrayList<ImageAperture> imageApertures) {
+                        PrintOrientation orientation, ArrayList<TextAperture> textAperture,
+                        ArrayList<ImageAperture> imageAperture) {
         super(cardType.getType(), color.getColor(), ProductSize.BUSINESS_CARD, getPrice(cardType, sides));
         this.sides = sides;
         this.printOrientation = orientation;
         this.paperType = getPaperType(cardType);
-        this.textApertures = textApertures;
-        this.imageApertures = imageApertures;
+        this.textApertures = textAperture;
+        this.imageApertures = imageAperture;
 
     }
     public BusinessCardSides getSides() {
