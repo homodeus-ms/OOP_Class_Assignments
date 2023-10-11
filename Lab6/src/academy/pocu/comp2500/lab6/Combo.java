582b156244cc2f32098e5d40968bcba506ed092c
X1_LAPTOP\seekc <seekchange@daum.net>
2023-10-11 09:47:36 +0900

    실습6 getMainCourse 일반화문제

--- Lab6/src/academy/pocu/comp2500/lab6/Combo.java
+++ Lab6/src/academy/pocu/comp2500/lab6/Combo.java
@@ -6,8 +6,9 @@ public class Combo extends Menu {
     protected static final int MAX_APPETIZER_COUNT = 2;
     protected static final int MAX_DESSERT_COUNT = 4;
 
+    private boolean isMainCourseExist;
     protected final ArrayList<Appetizer> appetizers = new ArrayList<>(MAX_APPETIZER_COUNT);
-    protected MainCourse mainCourse;
+    //protected MainCourse mainCourse;
     protected final ArrayList<Dessert> desserts = new ArrayList<>(MAX_DESSERT_COUNT);
 
     protected Combo(FoodType foodType) {
@@ -18,13 +19,17 @@ public class Combo extends Menu {
         assert (!appetizers.isEmpty()) : "set Appetizers First";
         return this.appetizers;
     }
-    public MainCourse getMainCourse() {
+    /*public MainCourse getMainCourse() {
         assert (mainCourse != null) : "set Main Course First";
         return this.mainCourse;
-    }
+    }*/
     public ArrayList<Dessert> getDesserts() {
         assert (!desserts.isEmpty()) : "set Desserts First";
         return this.desserts;
+    }
+
+    protected void setMainCourse() {
+        this.isMainCourseExist = true;
     }
 
     public boolean isValid() {
@@ -32,7 +37,7 @@ public class Combo extends Menu {
             case NO_HEAVY_MEAL:
                 return appetizers.size() == 2 && desserts.size() == 1;
             case THREE_COURSE_MEAL:
-                return appetizers.size() == 1 && mainCourse != null && desserts.size() == 1;
+                return appetizers.size() == 1 && isMainCourseExist && desserts.size() == 1;
             case DEATH_BY_DESSERTS:
                 return desserts.size() == 4;
             default:
--- Lab6/src/academy/pocu/comp2500/lab6/ThreeCourseMeal.java
+++ Lab6/src/academy/pocu/comp2500/lab6/ThreeCourseMeal.java
@@ -2,11 +2,15 @@ package academy.pocu.comp2500.lab6;
 
 public class ThreeCourseMeal extends Combo {
 
+    private MainCourse mainCourse;
     public ThreeCourseMeal() {
         super(FoodType.THREE_COURSE_MEAL);
     }
 
-
+    public MainCourse getMainCourse() {
+        assert (mainCourse != null) : "set MainCourse first";
+        return this.mainCourse;
+    }
 
     public void setAppetizer(Appetizer appetizer) {
         super.appetizers.clear();
@@ -14,6 +18,7 @@ public class ThreeCourseMeal extends Combo {
     }
     public void setMainCourse(MainCourse mainCourse) {
         this.mainCourse = mainCourse;
+        super.setMainCourse();
     }
     public void setDessert(Dessert dessert) {
         super.desserts.clear();
