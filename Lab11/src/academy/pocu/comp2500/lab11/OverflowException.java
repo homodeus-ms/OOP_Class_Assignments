74c0cddec08dcc878609582a64f77e3be7354ea7
X1_LAPTOP\seekc <seekchange@daum.net>
2023-11-29 16:39:19 +0900

    실습 11 두번째

--- Lab11/src/academy/pocu/comp2500/lab11/OverflowException.java
+++ Lab11/src/academy/pocu/comp2500/lab11/OverflowException.java
@@ -1,8 +1,9 @@
 package academy.pocu.comp2500.lab11;
 
 public class OverflowException extends RuntimeException {
-    private String message;
-    public OverflowException(String msg) {
-        this.message = msg;
+    //private String message;
+    private static final long serialVersionUID = 3L;
+    public OverflowException(final String msg) {
+        super(msg);
     }
 }
